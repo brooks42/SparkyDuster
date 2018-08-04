7d69afbd283d3869d49808384e255dacfdd09873
brooks42 <britainsux@gmail.com>
2018-08-03 23:47:02 -0700

    start building out the PN parsing architecture

--- boulder/index.js
+++ boulder/index.js
@@ -16,7 +16,15 @@ app.get('/', (req, res) => res.send('I am listening'));
 // on a post we'll want to use the CLI to compile and upload the posted code to an arduino on the robot
 app.post('/', (req, res) => { 
     console.log(`request: ${JSON.stringify(req.headers)} ${JSON.stringify(req.body)}`);
-    res.send() 
+    res.send();
+
+    // parse the github PN and decide whether we need to push or not
+    var notification = new PushNotification(req.body);
+    if (notification.shouldPullRepoAndRebuild()) {
+        console.log("Should pull the repo and rebuild...");
+
+        // do a bunch of stuff
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ boulder/pn_parser.js
@@ -0,0 +1,15 @@
+class PushNotification {
+
+    /**
+     * 
+     * @param {Object} obj a JSON object representing the body of the push notification
+     */
+    constructor(obj) {
+        console.log(`New PushNotification created wiht body ${JSON.stringify(obj)}`);
+        this.object = obj;
+    }
+
+    shouldPullRepoAndRebuild() {
+        // TODO: not sure how to implement this yet haha
+    }
+};
