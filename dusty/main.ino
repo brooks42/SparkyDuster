41dcb3d5079da31ce42583b9838b07ed7d7c559b
brooks42 <britainsux@gmail.com>
2018-08-11 20:42:15 -0700

    now the car can drive around by remote control. about to implement the rangefinder

--- dusty/main.ino
+++ dusty/main.ino
@@ -17,17 +17,57 @@ const int REMOTE_CONTROLLER_PIN = 12;
 #define OK_BUTTON 3622325019	// OK
 #define CONTINUE 4294967295 // repeated press registered
 
+/*define channel enable output pins*/
+#define ENA 5	  // Left  wheel speed
+#define ENB 10	  // Right wheel speed
+/*define logic control output pins*/
+#define IN1 6   // Left  wheel forward
+#define IN2 7   // Left  wheel reverse
+#define IN3 8   // Right wheel reverse
+#define IN4 9   // Right wheel forward
+#define carSpeed 150	// initial speed of car >=0 to <=255
+
 // remote_control_t remoteControl(REMOTE_CONTROLLER_PIN);
 IRrecv irrecv(REMOTE_CONTROLLER_PIN);  
 decode_results results;
+
+motor_t rightMotor(ENB, IN1, IN2);
+motor_t leftMotor(ENA, IN3, IN4);   
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(9600);
     
     pinMode(LED_BUILTIN, OUTPUT);
+    leftMotor.stop();
+    rightMotor.stop();
     
     irrecv.enableIRIn();  // Start receiving
+}
+
+void forward() {
+    leftMotor.forward();
+    rightMotor.forward();
+}
+
+void reverse() {
+    leftMotor.reverse();
+    rightMotor.reverse();
+}
+
+void stop() {
+    leftMotor.stop();
+    rightMotor.stop();
+}
+
+void left() {
+    leftMotor.forward();
+    rightMotor.reverse();
+}
+
+void right() {
+    leftMotor.reverse();
+    rightMotor.forward();
 }
 
 // the loop function runs over and over again forever
@@ -37,7 +77,36 @@ void loop() {
         unsigned long val = results.value;
         Serial.println(val);
 
+        // TODO: need a state maachine
+        // in the final codebase the raspi will handle the state machine component but for now just shove it in the arduino
+        if (val == FORWARD_BUTTON) {
+            Serial.println("forward");
+            stop();
+            forward();
+        }
+
+        if (val == BACK_BUTTON) {
+            Serial.println("reverse");
+            stop();
+            reverse();
+        }
+
+        if (val == LEFT_BUTTON) {
+            Serial.println("left");
+            stop();
+            left();
+        }
         
+        if (val == RIGHT_BUTTON) {
+            Serial.println("right");
+            stop();
+            right();
+        }
+
+        if (val == OK_BUTTON) {
+            Serial.println("stop");
+            stop();
+        }
         
         irrecv.resume();      // Receive the next value
         delay(100);
--- dusty/motor.h
+++ dusty/motor.h
@@ -1,4 +1,39 @@
+#include <Arduino.h>
+
 struct motor_t {
-    int pin;
-    uint8_t currentSpeed;
+    int _enablePin;
+    int _forwardPin;
+    int _reversePin;
+    int _currentSpeed = 0;
+
+    motor_t(int enablePin, int forwardPin, int reversePin): _enablePin(enablePin), _forwardPin(forwardPin), _reversePin(reversePin) {
+
+        // enable write mode
+        pinMode(_enablePin, OUTPUT);
+        pinMode(_forwardPin, OUTPUT);
+        pinMode(_reversePin, OUTPUT);
+
+        // stop the motor entirely
+        digitalWrite(_enablePin, LOW);
+        digitalWrite(_forwardPin, LOW);
+        digitalWrite(_reversePin, LOW);
+    }
+
+    void forward() {
+        digitalWrite(_enablePin, HIGH);
+        digitalWrite(_forwardPin, HIGH);
+        digitalWrite(_reversePin, LOW);
+    }
+
+    void reverse() {
+        digitalWrite(_enablePin, HIGH);
+        digitalWrite(_forwardPin, LOW);
+        digitalWrite(_reversePin, HIGH);
+    }
+
+    void stop() {
+        digitalWrite(_enablePin, LOW);
+        digitalWrite(_forwardPin, LOW);
+        digitalWrite(_reversePin, LOW);
+    }
 };
